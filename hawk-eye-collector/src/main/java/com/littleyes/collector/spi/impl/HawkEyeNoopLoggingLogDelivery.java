814d8ae6e1403ce06bdae967e4695895c77c3634
陈俊兵 <chenjunbing@mhqcedu.cn>
2021-02-23 13:54:41 +0800

    adjust

---
+++ hawk-eye-collector/src/main/java/com/littleyes/collector/spi/impl/HawkEyeNoopLoggingLogDelivery.java
@@ -0,0 +1,15 @@
+package com.littleyes.collector.spi.impl;
+
+import com.littleyes.collector.spi.LoggingLogDelivery;
+import com.littleyes.common.core.SPI;
+
+/**
+ * <p> <b> 默认 Logging 日志传输器接口实现 </b> </p>
+ *
+ * @author Junbing.Chen
+ * @date 2021-02-19
+ */
+@SPI
+public class HawkEyeNoopLoggingLogDelivery implements LoggingLogDelivery {
+
+}
