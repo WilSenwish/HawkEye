a2f752c6e2e39b8635db75ba6bd060c18d825acd
陈俊兵 <chenjunbing@mhqcedu.cn>
2021-03-24 10:35:45 +0800

    add global exception handler

---
+++ hawk-eye-manager/src/main/java/com/littleyes/manager/util/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.littleyes.manager.util;
+
+import com.littleyes.common.util.ApiException;
+import com.littleyes.common.util.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * <p> <b> 全局异常处理 </b> </p>
+ *
+ * @author Junbing.Chen
+ * @date 2021-03-24
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ApiException.class)
+    @ResponseStatus(value = HttpStatus.OK)
+    public ApiResponse handleApiException(Exception e) {
+        return ApiResponse.failure(e.getMessage());
+    }
+
+}
